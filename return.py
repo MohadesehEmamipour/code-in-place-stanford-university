f9a6039bca2e0e35b72ddf3e57fb4ecbf3ca9f83
Mohadeseh <emamipourmohadeseh@yahoo.com>
2022-06-02 15:47:31 +0430

    Add files via upload

---
+++ return.py
@@ -0,0 +1,18 @@
+'''
+def meters_to_cm_case1(meters):
+    return 100*meters
+
+def meters_to_cm_case2(meters):
+    print(100*meters)
+
+def main():
+    meters_to_cm_case2(5)
+'''
+
+
+def meters_to_cm(meters):
+     return 100*meters
+
+def main():
+    result=meters_to_cm(5.2)
+    print(result)
